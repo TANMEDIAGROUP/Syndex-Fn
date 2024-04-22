81466d01167738437333889afc56acab161c35e1
Jensen Ruan <jruan@Jensens-MacBook-Pro.local>
2024-04-22 02:59:34 -0400

    Implemented Redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "./App.css";
+//import store from './store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
